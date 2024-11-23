214632eaa86ebe7cc7e7d5d738ed3396cb8feffd
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-11-23 09:44:55 -0600

    simple query working

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
+import http.client
+import json
+
 if __name__ == '__main__':
-    pass
+    conn = http.client.HTTPSConnection('api.exchange.coinbase.com')
+    payload = ''
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'python'
+    }
+    conn.request('GET', '/currencies/DOGE', payload, headers)
+    resp = conn.getresponse()
+    data = resp.read()
+    print(data.decode('utf-8'))
