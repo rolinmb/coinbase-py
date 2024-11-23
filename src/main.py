51ceac7d4439ef3b77ed0aff44d61796e6f71906
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-11-23 10:16:45 -0600

    now we cookin

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
+from util import check_user_input
 import http.client
 import json
+import sys
 
 if __name__ == '__main__':
-    conn = http.client.HTTPSConnection('api.exchange.coinbase.com')
-    payload = ''
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'python'
-    }
-    conn.request('GET', '/currencies/DOGE', payload, headers)
-    resp = conn.getresponse()
-    data = resp.read()
-    print(data.decode('utf-8'))
+    if len(sys.argv) != 2:
+        print('src/main.py : __main__ :: ERROR ::: Must enter only one cryptocurrency name as a command line argument\n-> EXAMPLE USAGE (from root): python src/main.py BTC')
+        sys.exit(1)
+    try :
+        crypto_name = check_user_input(sys.argv[1])
+        conn = http.client.HTTPSConnection('api.exchange.coinbase.com')
+        payload = ''
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': 'python/https.client'
+        }
+        conn.request('GET', f'/currencies/{crypto_name}', payload, headers)
+        resp = conn.getresponse()
+        data = resp.read().decode('utf-8', errors='ignore')
+        json_data = json.loads(data)
+        print(json.dumps(json_data, indent=4))
+    except ValueError as e:
+        print(f'src/main.py : __main__ :: ERROR ::: {e}')
+        sys.exit(1)
+    except Exception as e:
+        print(f'src/main.py : __main__ :: ERROR ::: {e}')
+        sys.exit(1)
---
+++ src/util.py
@@ -0,0 +1,8 @@
+def check_user_input(crypto_name):
+    if len(crypto_name) < 2:
+        raise ValueError('check_user_input() :: ERROR ::: Cryptocurrency name must be at least 3 alphabetical characters.')
+    if len(crypto_name) > 5:
+        raise ValueError('check_user_input() :: ERROR ::: Cryptocurrency name must be at most 5 alphabetical characters.')
+    if not crypto_name.isalpha():
+        raise ValueError('check_user_input() :: ERROR ::: Cryptocurrency name must only contain alphabetical characters.')
+    return crypto_name.upper()
