72d4e108751d00053039fad1ea7c6bf7bd7a3297
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-11-23 10:17:51 -0600

    README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Interacting with the Coinbase REST API via python.
+
+Run from root: python src/main.py CRYPTONAME ( 2 < len(CRYPTONAME) > 6)
--- src/util.py
+++ src/util.py
@@ -1,7 +1,7 @@
 def check_user_input(crypto_name):
     if len(crypto_name) < 2:
         raise ValueError('check_user_input() :: ERROR ::: Cryptocurrency name must be at least 3 alphabetical characters.')
-    if len(crypto_name) > 5:
+    if len(crypto_name) > 6:
         raise ValueError('check_user_input() :: ERROR ::: Cryptocurrency name must be at most 5 alphabetical characters.')
     if not crypto_name.isalpha():
         raise ValueError('check_user_input() :: ERROR ::: Cryptocurrency name must only contain alphabetical characters.')
